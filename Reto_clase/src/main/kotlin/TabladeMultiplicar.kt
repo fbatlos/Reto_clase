a9c10d4d1d5369cfec9a476c79eea0dba3f4781e
fbatlos <bdlsfrancisco@iesrafaelalberti.es>
2024-01-10 17:25:44 +0100

    multi

--- Reto_clase/src/main/kotlin/TabladeMultiplicar.kt
+++ Reto_clase/src/main/kotlin/TabladeMultiplicar.kt
@@ -1,3 +1,6 @@
+import kotlin.math.max
+import kotlin.math.min
+
 /**
  * Solicita un número entero en un rango.
  *
@@ -8,8 +11,13 @@
  * @return Int - número entero válido dentro del rango especificado
  */
 fun pedirNum(): Int {
+    print("Introducción de un número entre 1 y 100 : ")
     val numero = readln().toInt()
-    return numero
+    if (numero>=1 && numero<=100){
+        return numero
+    }else{
+        error(" ")
+    }
 }
 
 /**
@@ -21,18 +29,35 @@ fun pedirNum(): Int {
  */
 
 
-fun pregunta(){
-
+fun pregunta(): Boolean {
+    print("Quieres hacerlo otra vez ? s/si ó n/no: ")
+    val comprobante=readln().lowercase()
+    if (comprobante == "s" || comprobante == "si"){
+        return true
+    }else
+    if (comprobante == "n" || comprobante == "no"){
+        print("Adios")
+        return false
+    }else{
+        error(" ")
+    }
 }
 
 fun main() {
     //TODO: Solicitar la introducción de un número entre 1 y 100 y mostrar su table de multiplicar...
-    print("Introducción de un número entre 1 y 100 : ")
-    val numero = pedirNum()
-    println("La tabla de multiplicar del número ${numero} es: ")
-    val tabla  = IntArray(10 ){i -> i + 1 }
-    for (i in tabla){
-        println("${i} -> ${numero} x ${i} = ${i*numero}")
-    }
+   try {
+       var repuesta = true
+       do {
+           val numero = pedirNum()
+           println("La tabla de multiplicar del número ${numero} es: ")
+           val tabla = IntArray(10) { i -> i + 1 }
+           for (i in tabla) {
+               println("${i} -> ${numero} x ${i} = ${i * numero}")
+           }
+           repuesta = pregunta()
+       }while (repuesta == true)
+   }catch (_:Exception){
+       println("**ERROR**")
+   }
     //Hasta que se responda negativamente a la pregunta "¿Desea generar otra tabla de multiplicación? (s/n)"
 }
