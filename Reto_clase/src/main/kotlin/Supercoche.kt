f7ad60e40b16a590304f00faf54c9b99f5afdf2d
fbatlos <bdlsfrancisco@iesrafaelalberti.es>
2024-01-16 13:02:31 +0100

    Calculadora

--- Reto_clase/src/main/kotlin/Supercoche.kt
+++
@@ -1,7 +0,0 @@
-internal open class supercoche(protected var marca: String?, protected var añocompra: Int, protected var precio: Int) {
-    open fun mostrarDatos() {
-        println("Marca: $marca")
-        println("Año de compra: $añocompra")
-        println("Precio: $precio")
-    }
-}
